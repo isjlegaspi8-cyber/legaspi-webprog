1a38a3354c4668c78f417c5b5d12a3e8ec9bb383
Isam Legaspi <isamlegaspi6@gmail.com>
2026-04-16 21:16:15 +0800

    lab-act-instance

---
+++ legaspi-client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Isam Legaspi<br />
+          Email: isjlegaspi8@gmail.com<br />
+          Other Personal Info: BSIT Student at NU Manila
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
